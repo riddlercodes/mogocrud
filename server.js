f37b39311aeda0b89b95e24786f0dc75309b36b8
riddler <riddlerocdes@gmail.com>
2017-12-27 16:51:19 -0500

    test

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+        // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+        // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
